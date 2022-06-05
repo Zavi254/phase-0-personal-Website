91036fdedda54172c2792091c4d28b24c73fb236
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-05 20:16:43 +0300

    made the cards rotatable when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,29 @@ closeButton.addEventListener("click", function(){
     closeButton.style.display = "none";
     mobileHiddenLinks.style.display = "none";
 });
+
+// Animation for the cards
+
+const firstCard = document.getElementById('firstCard');
+const secondCard = document.getElementById('secondCard');
+const thirdCard = document.getElementById('thirdCard');
+
+
+firstCard.addEventListener("click", function(){
+    firstCard.style.animationName = 'rotate';
+    firstCard.style.animationDuration = '3s';
+});
+
+secondCard.addEventListener("click", function(){
+    secondCard.style.animationName = 'rotate';
+    secondCard.style.animationDuration = '3s';
+});
+
+thirdCard.addEventListener("click", function(){
+    thirdCard.style.animationName = 'rotate';
+    thirdCard.style.animationDuration = '3s';
+})
+
+const thankYou = () => {
+    alert("Kindly wait for a response!")
+}
