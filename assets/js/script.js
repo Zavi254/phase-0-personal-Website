cf6ac928a6fa56dd314c52fdfd89d42fc885ae47
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-05 16:19:28 +0300

    made the button toggler in the navigation bar in the mobile device active when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-// const h2  =document.createElement("h2");
-// h2.textContent = "This content added by Javascript";
+// Navigation button for the mobile toggler
 
-// document.querySelector("body").appendChild(h2);
+const buttonToggler = document.getElementById('buttonToggler');
+const closeButton = document.getElementById('closeButton');
+const mobileHiddenLinks = document.getElementById('desktopNavbarSection');
+
+buttonToggler.addEventListener("click", function(){
+    closeButton.style.display = "block";
+    buttonToggler.style.display = "none";
+    mobileHiddenLinks.style.display = "block"
+});
+
+closeButton.addEventListener("click", function(){
+    buttonToggler.style.display  ="block";
+    closeButton.style.display = "none";
+    mobileHiddenLinks.style.display = "none";
+});
